48c6d7af1b4d17a8cb6287c38877fda07fecaef4
alice <alice@valour.me>
2017-05-18 18:01:21 +0800

    parse options to show buttons and send message for options

--- iOSKinkedIn/CareConvoVC.swift
+++ iOSKinkedIn/CareConvoVC.swift
@@ -13,7 +13,9 @@ enum ReplyType {
     case none, text, choice
 }
 
-class CareConvoVC: ATLConversationViewController, ATLConversationViewControllerDataSource, ATLConversationViewControllerDelegate {
+class CareConvoVC: ATLConversationViewController,
+    ATLConversationViewControllerDataSource,
+    ATLConversationViewControllerDelegate{
     
     private var dateFormatter = DateFormatter()
     private var replyState: ReplyType = .text
@@ -22,11 +24,20 @@ class CareConvoVC: ATLConversationViewController, ATLConversationViewControllerD
     
     private let LABEL_HEIGHT = 30
     private let LABEL_PADDING = 8
-
+    
+    private var currentQuestion: [String: Any]? = nil
+    private var tempQ: String? = nil
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.navigationController?.setNavigationBarHidden(false, animated: false)
         
+        /*
         print("client ready \(LayerHelper.client != nil)")
         do {
             self.layerClient = LayerHelper.client!
@@ -35,6 +46,7 @@ class CareConvoVC: ATLConversationViewController, ATLConversationViewControllerD
         } catch {
             print("failed to start aftercare convo")
         }
+        */
         
         dateFormatter.dateStyle = .short
         dateFormatter.timeStyle = .short
@@ -47,8 +59,13 @@ class CareConvoVC: ATLConversationViewController, ATLConversationViewControllerD
         self.messageInputToolbar.displaysRightAccessoryImage = false
         self.messageInputToolbar.leftAccessoryButton = nil
         
+        NotificationCenter.default.addObserver(self,
+            selector: #selector(didReceiveLayerObjectsDidChangeNotification),
+            name: NSNotification.Name.LYRClientObjectsDidChange,
+            object: self.layerClient)
+        
         _makeOptionsView()
-        _setOptions(["No", "Never"])
+        
     }
     
     private func _makeOptionsView() {
@@ -72,11 +89,15 @@ class CareConvoVC: ATLConversationViewController, ATLConversationViewControllerD
         stackHeightConstraint = height
     }
     
-    private func _setOptions(_ opts: [String]){
+    private func _closeOptionsView(){
         // Remove exsiting elements not tested yet
         for sv in stack.arrangedSubviews {
             sv.removeFromSuperview()
         }
+        stack.isHidden = true
+    }
+    
+    private func _setOptions(_ opts: [String]){
         
         for o in opts {
             let lbl = UIButton()
@@ -101,6 +122,20 @@ class CareConvoVC: ATLConversationViewController, ATLConversationViewControllerD
     
     func optionTapped(_ sender: UIButton){
         print("response \(sender.title(for: .normal))")
+        guard let text = sender.title(for: .normal) else {
+            print("cant find")
+            return
+        }
+        let part = LYRMessagePart(text: text)
+        do {
+            let msg = try layerClient.newMessage(with: [part], options: nil)
+            try self.conversation.send(msg)
+            
+            _closeOptionsView()
+
+        } catch {
+            print("error in sending message")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -111,6 +146,7 @@ class CareConvoVC: ATLConversationViewController, ATLConversationViewControllerD
     public func conversationViewController(_ conversationViewController: ATLConversationViewController, participantFor identity: LYRIdentity) -> ATLParticipant {
         return identity
     }
+    
     
     public func conversationViewController(_ conversationViewController: ATLConversationViewController, attributedStringForDisplayOf date: Date) -> NSAttributedString {
         let dict = [
@@ -125,7 +161,7 @@ class CareConvoVC: ATLConversationViewController, ATLConversationViewControllerD
     }
     
     public func conversationViewController(_ viewController: ATLConversationViewController, didSend message: LYRMessage) {
-        updateReplyState(state: .none)
+        //updateReplyState(state: .none)
     }
     
     public func updateReplyState(state: ReplyType){
@@ -140,6 +176,62 @@ class CareConvoVC: ATLConversationViewController, ATLConversationViewControllerD
             self.messageInputToolbar.isHidden = true
         default:
             self.messageInputToolbar.isHidden = true
+        }
+    }
+    
+    func didReceiveLayerObjectsDidChangeNotification(_ notification: NSNotification){
+        print("something change")
+        guard let info = notification.userInfo else {
+            return
+        }
+        
+        guard let changes = info[LYRClientObjectChangesUserInfoKey] as? NSArray else {
+            return
+        }
+        for ch in changes{
+            if let change = ch as? LYRObjectChange {
+                if let message = isMessageRecieved(change) {
+                    interceptMessage(message)
+                }
+            }
+        }
+    }
+    
+    private func isMessageRecieved(_ change: LYRObjectChange) -> LYRMessage? {
+        if(change.type == .create){
+            if let message = change.object as? LYRMessage {
+                if(message.sender.userID=="aftercare"){
+                    return message
+                }
+            }
+        }
+        return nil
+    }
+    
+    private func interceptMessage(_ message: LYRMessage){
+        print("interecepted message")
+        if(message.parts.count > 1 && message.parts.last?.mimeType == "application/json"){
+            if let headerData = message.parts.last?.data {
+                do {
+                    currentQuestion = try JSONSerialization.jsonObject(with: headerData, options: .mutableContainers) as? [String: Any]
+                    if let responseType = currentQuestion?["response_type"] as? String {
+                        if (responseType == "choice"){
+                            if let options = currentQuestion?["options"] as? [String] {
+                                _setOptions(options)
+                            }
+                        }
+                        else if (responseType == "none"){
+                            updateReplyState(state: .none)
+                        }
+                        else {
+                            updateReplyState(state: .text)
+                        }
+                    }
+                } catch {
+                    print(error)
+                }
+            }
+            
         }
     }
 
---
+++ iOSKinkedIn/HelpIntroVC.swift
@@ -0,0 +1,57 @@
+//
+//  HelpIntroVC.swift
+//  iOSKinkedIn
+//
+//  Created by alice on 5/16/17.
+//  Copyright © 2017 KinkedIn. All rights reserved.
+//
+
+import UIKit
+
+class HelpIntroVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func chatWithKia(_ sender: Any) {
+        let convo = CareConvoVC(layerClient: LayerHelper.client!)
+        do{
+            convo.conversation = try LayerHelper.startConvo(withUser: "aftercare")
+            convo.hidesBottomBarWhenPushed = true
+            self.navigationController?.pushViewController(convo, animated: false)
+        } catch {
+            print("failed to start aftercare convo")
+        }
+        
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        /*
+        if(segue.identifier=="help2chat"){
+            if let convoView = segue.destination as? CareConvoVC {
+                do {
+                    convoView.layerClient = LayerHelper.client!
+                    convoView.conversation = try LayerHelper.startConvo(withUser: "aftercare")
+                    print("set conversation \(convoView.conversation)")
+                } catch {
+                    print("failed to start aftercare convo")
+                }
+            }
+        }
+        */
+    }
+    
+}
